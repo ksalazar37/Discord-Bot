20eb0f6972ed1d1101491167f0ff36b0c7f2874d
Kristina Salazar <31550880+ksalazar37@users.noreply.github.com>
2019-02-24 15:45:45 -0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+# main discord bot script
+# initiates all other script with ran
+
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+# list of names of extensions of bot
+startup_extensions=["reddit", "wolf_bot", "play", "8ball","message_reply"]
+bot = commands.Bot(command_prefix='!')
+
+# prefix before entering a command 
+BOT_PREFIX = ("?", "!")
+TOKEN = "token"
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__,e)
+            print('failed to load ext'.format(extension, exc))
+
+bot.run("TOKEN")
